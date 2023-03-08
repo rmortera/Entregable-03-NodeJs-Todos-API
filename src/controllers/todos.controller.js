8b2db39451b45f78205a59e9b4514c5da200e535
robmortera93 <robmrtra@gmail.com>
2023-03-08 15:03:50 -0600

    update

--- src/controllers/todos.controller.js
+++ src/controllers/todos.controller.js
@@ -1,19 +1,19 @@
 const TodosServices = require("../services/todos.services");
 
-const getAllTodos = async (req, res) => {
-  try {
-    const todos = await TodosServices.getAll();
-    res.json(todos);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-};
-
 const createTodo = async (req, res) => {
   try {
     const newTodo = req.body;
     const todo = await TodosServices.create(newTodo);
     res.status(201).json(todo);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const getAllTodos = async (req, res) => {
+  try {
+    const todos = await TodosServices.getAll();
+    res.json(todos);
   } catch (error) {
     res.status(400).json(error);
   }
